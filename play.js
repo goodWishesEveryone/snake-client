a2599f68dc6bc2df54d946a5d14a2a22d8f186dc
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2021-12-05 19:56:42 -0500

    Modified client.js to exit process when key (CTRL+C) is pressed

--- play.js
+++ play.js
@@ -1,6 +1,27 @@
 const net = require("net");
 const connect = require("./client");
 
-
 console.log("Connecting ...sm");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+// process.stdin.on('data', (key) => {
+//   process.stdout.write('.');
+// });
+// console.log('after callback');
+
+const handleUserInput = function (key) {
+  //unicode CRTL + C
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
